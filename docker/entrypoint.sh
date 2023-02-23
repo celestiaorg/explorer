318fbf6fc057905929a5e2b7d5a9af6915ddcb2f
Jose Ramon Mañes <jose@celestia.org>
2023-02-23 16:12:44 +0100

    feat: remove default values

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,40 +1,4 @@
 #!/bin/bash
-PATH_TO_CHAINS="/home/${USR_NAME}/src/chains/"
-DEFAULT_CHAIN_NAME="arabica"
-
-whoami
-
-# check if the chains folder is empty, if so, we generate a default one
-if [ -z "$(ls -A $PATH_TO_CHAINS/mainnet/)" ]; then
-    echo "================================================"
-    echo "Not found any file, creating the default one..."
-    echo "================================================"
-    mkdir -p ${PATH_TO_CHAINS}/mainnet/
-    cat <<EOF > ${PATH_TO_CHAINS}/mainnet/${DEFAULT_CHAIN_NAME}.json
-{
-  "chain_name": "arabica",
-  "coingecko": "",
-  "api": ["https://api.limani.celestia-devops.dev"],
-  "rpc": ["https://rpc.limani.celestia-devops.dev"],
-  "sdk_version": "0.46.1",
-  "coin_type": "118",
-  "min_tx_fee": "300",
-  "addr_prefix": "utia",
-  "logo": "/logos/androma.png",
-  "assets": [
-    {
-      "base": "uandr",
-      "symbol": "ANDR",
-      "exponent": "6",
-      "coingecko_id": "",
-      "logo": "/logos/celestia.png"
-    }
-  ]
-}
-EOF
-cp ${PATH_TO_CHAINS}/mainnet/arabica.json ${PATH_TO_CHAINS}/testnet/arabica.json
-fi
-
 echo "================================================"
 echo "[INFO] Installing packages..."
 echo "================================================"
